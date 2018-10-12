29298cc36c7059e0a99b57d3277805368e79d410
AlexPierov <pesasha@gmail.com>
2018-10-12 15:20:46 +0300

    added write email

--- Test Suites/1.ts
+++ Test Suites/1.ts
@@ -4,7 +4,7 @@
    <name>1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-10-12T14:11:45</lastRun>
+   <lastRun>2018-10-12T15:12:07</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
